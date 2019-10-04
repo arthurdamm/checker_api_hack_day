e22dd0b3a9773eaef123b1ef6d8ee9c3ef71c738
Laura Roudge <laura.derohan@gmail.com>
2019-10-04 13:37:58 -0700

    Add congratulations messages

--- js/message.js
+++ js/message.js
@@ -1,4 +1,10 @@
 const messageDict = {
+    'You have all green checks!': [
+        'Congratulations :)',
+        'Good work buddy!',
+        'Wow, that hard work really paid off!',
+        'Nice job! Now go help your peers ;)'
+    ],
     'You have all red checks.': [
         'Is your GitHub repo set up with the correct name? The checker might not find it.',
         'Is the file pushed to the master branch? The checker clones your repo from master.',
