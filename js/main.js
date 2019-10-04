89e4b2958008acf76f30b2e86b492e6105ba25f8
Arthur Damm <arthurdamm@Arthurs-MacBook-Air.local>
2019-10-04 00:13:02 -0700

    Refactor to keep Tu happy

--- js/main.js
+++ js/main.js
@@ -4,78 +4,73 @@ $(function () {
 
   console.log('CheckerBuddy Loading...');
 
-  $('button#login_button').click(function() {
-    console.log("LOGIN");
-    const email = $('input[name=email]').val();
-    const password = $('input[name=password]').val();
+  $('button#login_button').click(function () {
+    console.log('LOGIN');
     const json = {};
     json.api_key = '96a4134e30845c1fe3ed6e016f19e423';
-    json.email = email;
-    json.password = password;
-    json.scope = "checker";
- 
-    let authenticationRequest = {
-      "async": true,
-      "crossDomain": true,
-      "url": "https://cors-anywhere.herokuapp.com/https://intranet.hbtn.io/users/auth_token.json",
-      "method": "POST",
-      "headers": {
-        "Content-Type": "application/json",
+    json.email = $('input[name=email]').val();
+    json.password = $('input[name=password]').val();
+    json.scope = 'checker';
+
+    const authenticationRequest = {
+      async: true,
+      crossDomain: true,
+      url: 'https://cors-anywhere.herokuapp.com/https://intranet.hbtn.io/users/auth_token.json',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
       },
-      "data": JSON.stringify(json)
-    }
-    $.ajax(authenticationRequest).done(function (response) {
-      console.log(response);
-      session.auth_token = response.auth_token;
-      console.log("SESSION:", session);
+      data: JSON.stringify(json)
+    };
+    $.ajax(authenticationRequest).done(function (data) {
+      console.log('AUTH:', data);
+      session.auth_token = data.auth_token;
     });
   });
 
-  $('button#project_button').click(function() {
-    const pid = $('input[name=project]').val() || "300";
-    console.log("PROJECT:", pid);
+  $('button#project_button').click(function () {
+    const projectId = $('input[name=project]').val();
+    console.log('Play:', projectId);
 
-    let projectRequest = {
-      "async": true,
-      "crossDomain": true,
-      "url": `https://cors-anywhere.herokuapp.com/https://intranet.hbtn.io/projects/${pid}.json?auth_token=${session.auth_token}`,
-      "method": "GET",
-    }
+    const projectRequest = {
+      async: true,
+      crossDomain: true,
+      url: `https://cors-anywhere.herokuapp.com/https://intranet.hbtn.io/projects/${projectId}.json?auth_token=${session.auth_token}`,
+      method: 'GET'
+    };
     $.ajax(projectRequest).done(function (data) {
-      console.log("PROJECT:", data);
+      console.log('PROJECT:', data);
       $('#task_header').html(data.name);
       $('#task_list').empty();
       for (const task of data.tasks) {
-        $('#task_list').append(`<li class="task" task-id="${task.id}"">${task.title}</li>`);
+        $('#task_list').append(`<li class='task' task-id='${task.id}''>${task.title}</li>`);
       }
     });
   });
 
   $(document).on('click', 'li.task', function () {
-    const taskId = $(this).attr("task-id");
-    console.log("TASK:", taskId);
+    const taskId = $(this).attr('task-id');
+    console.log('TASK:', taskId);
 
-    let correctionRequest = {
-      "async": true,
-      "crossDomain": true,
-      "url": `https://cors-anywhere.herokuapp.com/https://intranet.hbtn.io/tasks/${taskId}/start_correction.json?auth_token=${session.auth_token}`,
-      "method": "POST",
-    }
+    const correctionRequest = {
+      async: true,
+      crossDomain: true,
+      url: `https://cors-anywhere.herokuapp.com/https://intranet.hbtn.io/tasks/${taskId}/start_correction.json?auth_token=${session.auth_token}`,
+      method: 'POST'
+    };
     $.ajax(correctionRequest).done(function (data) {
-      console.log("CORRECTION:", data);
+      console.log('CORRECTION:', data);
       session.tasks[taskId] = data.id;
-  
-      let resultRequest = {
-      "async": true,
-      "crossDomain": true,
-      "url": `https://cors-anywhere.herokuapp.com/https://intranet.hbtn.io/correction_requests/${data.id}.json?auth_token=${session.auth_token}`,
-      "method": "GET",
-      }
-      $.ajax(resultRequest).done(function (data) {
-        console.log("RESULT:", data);
 
+      const resultRequest = {
+        async: true,
+        crossDomain: true,
+        url: `https://cors-anywhere.herokuapp.com/https://intranet.hbtn.io/correction_requests/${data.id}.json?auth_token=${session.auth_token}`,
+        method: 'GET'
+      };
+      $.ajax(resultRequest).done(function (data) {
+        console.log('RESULT:', data);
       });
     });
   });
-
 });
